53cad8fb56a1fe14933b8aa120d289f2d58d5b13
Lee Min Waan <100744636+plsgivemeachane@users.noreply.github.com>
2025-07-04 13:56:21 +0700

    Bruh the gitignore

---
+++ src/layer0/node/events/impl/chain_event/bft_block_event.py
@@ -0,0 +1,95 @@
+from layer0.utils.crypto.signer import SignerFactory
+from math import floor
+from layer0.blockchain.core.block import Block
+from rich import inspect
+from ecdsa import VerifyingKey, SigningKey, BadSignatureError, SECP256k1
+
+from layer0.blockchain.core.validator import Validator
+from layer0.blockchain.processor.block_processor import BlockProcessor
+from layer0.node.events.EventHandler import EventHandler
+from layer0.node.events.node_event import NodeEvent
+
+class BFTBlockEvent(EventHandler):
+    @staticmethod
+    def event_name() -> str:
+        return "bft_confirm"
+    
+    @staticmethod
+    def require_field() -> list[str]:
+        return ["block", "receipts_root", "signatures", "address", "publicKey"]
+    
+    def contain_in_pool(self, block: Block) -> bool:
+
+        return block.hash in self.neh.node.blockchain.block_bft_sign
+    
+    def get_block_in_pool(self, block_hash: str) -> Block | None:
+        block: Block | None = None
+        for block in self.neh.node.blockchain.block_bft_pool:
+            if block.hash == block_hash:
+                return block
+            
+        return None
+    
+    def handle(self, event: "NodeEvent") -> bool:
+        print("Send from "+ str(event.origin))
+        block = event.data["block"]
+        receipts_root: str = event.data["receipts_root"]
+        if isinstance(block, str):
+            block: Block = BlockProcessor.cast_block(event.data["block"])
+            
+        if not self.contain_in_pool(block):
+            print("bft_block_event.py:handle: Block is not in pool")
+            return False
+        
+        # print(event.origin)
+        # inspect(block)
+        
+        pending_block = self.get_block_in_pool(block.hash)
+        
+        if not pending_block:
+            return False # Cannot find pending block in pool
+        
+        # Check my receipts root
+        
+        if pending_block.receipts_root != receipts_root:
+            print("bft_block_event.py:handle: Receipts root does not match")
+            return False
+        
+        blockchain_instance = self.neh.node.blockchain
+        
+        sign: str = event.data["signatures"]
+        address: str = event.data["address"]
+        publicKey: str = event.data["publicKey"]
+        pk_obj: VerifyingKey = SignerFactory().get_signer().deserialize(publicKey)
+        
+        # is the address is indeed with the public key?
+        if not SignerFactory().get_signer().address(pk_obj) == address:
+            print("bft_block_event.py:handle: Address does not match")
+            return False
+        
+        # Is the address is the validator?
+        if address not in self.neh.node.worldState.get_validators():
+            print("bft_block_event.py:handle: Address is not a validator")
+            return False
+        
+        # Is the signature valid?
+        if not SignerFactory().get_signer().verify(block.get_string_for_signature(), sign, pk_obj):
+            print("bft_block_event.py:handle: Signature is not valid")
+            return False
+        
+        # Is the signature already exist?
+        if sign in blockchain_instance.block_bft_sign[block.hash]:
+            print("bft_block_event.py:handle: Signature already exist")
+            return False
+        
+        blockchain_instance.block_bft_sign[block.hash].append(sign)
+        
+        print(f"bft_block_event.py:handle: Block #{block.index} has a vote!")
+        print(floor(len(self.neh.node.worldState.get_validators()) / 1.5))
+        # print(blockchain_instance.block_bft_count[block.hash])
+        
+        if len(blockchain_instance.block_bft_sign[block.hash]) >= floor(len(self.neh.node.worldState.get_validators()) / 1.5): 
+            blockchain_instance.finalize_block(block)
+            # return False
+        
+        return True
